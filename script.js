f80fc4e8b1f7777f8ca80c5d5bb0544acc6c0991
Matt Perry <solidforge@gmail.com>
2020-12-08 03:47:31 -0700

    First publish!
    The website has styling and basic introduction to me.

--- script.js
+++ script.js
@@ -2,19 +2,13 @@
 //Open header when clicking the hamburger
 $('.hamburger').click(function () {
     $('header').addClass('open');
-});
-
-$('.hamburger').click(function () {
-    $('.ribbontext').addClass('expand');
+    $('.ribbon-text').addClass('expand');
 });
 
 //Close header by clicking the X
 $('.close').click(function () {
     $('header').removeClass('open');
-});
-
-$('.close').click(function () {
-    $('.ribbontext').removeClass('expand');
+    $('.ribbon-text').removeClass('expand');
 });
 
 //Close navigation menu when tapping outside
