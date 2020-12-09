f06f1ed2c34c401a7dc561e43a529637b334d35f
Matt Perry <solidforge@gmail.com>
2020-12-09 10:02:15 -0700

    Added updated portfolio and improved styles
    Fixed scrolling in webpages, and added more to portfolio

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+const moodPicker = document.getElementById('mood-picker');
+const body = document.getElementById('body');
+const scrollBar = document.getElementById('::-webkit-scrollbar');
+const scrollBarThumb = document.getElementById('::-webkit-scrollbar-thumb');
+const html = document.getElementById('html');
+
 //MOBILE NAV JQUERY
 //Open header when clicking the hamburger
 $('.hamburger').click(function () {
     $('header').addClass('open');
     $('.ribbon-text').addClass('expand');
+    body.style.height = '100%';
+    body.style.overflowY = 'hidden';
+    body.style.borderRight = 'solid 15px var(--dark-background)';
 });
 
 //Close header by clicking the X
 $('.close').click(function () {
     $('header').removeClass('open');
     $('.ribbon-text').removeClass('expand');
+    body.style.height = 'initial';
+    body.style.overflowY = 'scroll';
+    body.style.borderRight = 'none';
 });
 
 //Close navigation menu when tapping outside
@@ -18,7 +30,6 @@ $('main').click(function () {
 
 //MOBILE NAV JQUERY END
 
-let moodPicker = document.getElementById('mood-picker');
 
 function mouseOverMoods() {
     moodPicker.classList.add('fade-in');
