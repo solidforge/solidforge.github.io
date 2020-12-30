bdc6f17e385257b91759b93ecd62d21c66907c5d
Matt Perry <solidforge@gmail.com>
2020-12-29 22:51:13 -0700

    Added animation, margins
    Added CSS animation to scale up and down website on the portfolio. Also added larger margins to section elements.

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
-const moodPicker = document.getElementById('mood-picker');
+//const moodPicker = document.getElementById('mood-picker');
+const header = document.getElementById('header');
 const body = document.getElementById('body');
 const scrollBar = document.getElementById('::-webkit-scrollbar');
 const scrollBarThumb = document.getElementById('::-webkit-scrollbar-thumb');
-const html = document.getElementById('html');
+const wide = window.matchMedia('(min-width: 1024px)');
 
 //MOBILE NAV JQUERY
 //Open header when clicking the hamburger
-$('.hamburger').click(function () {
-    $('header').addClass('open');
-    $('.ribbon-text').addClass('expand');
+function openNav() {
+    header.classList.add('open');
+    //app.ribbonText.addClass('expand');
     body.style.height = '100%';
     body.style.overflowY = 'hidden';
-    body.style.borderRight = 'solid 15px var(--dark-background)';
-});
+    if(wide.matches) body.style.borderRight = 'solid 15px var(--dark-background)';
+}
 
-//Close header by clicking the X
-$('.close').click(function () {
-    $('header').removeClass('open');
-    $('.ribbon-text').removeClass('expand');
+//Close header by clicking the X or tapping in document
+function closeNav() {
+    header.classList.remove('open');
+    //app.ribbonText.removeClass('expand');
     body.style.height = 'initial';
     body.style.overflowY = 'scroll';
-    body.style.borderRight = 'none';
-});
-
-//Close navigation menu when tapping outside
-$('main').click(function () {
-    $('header').removeClass('open');
-});
+    if(wide.matches) body.style.borderRight = 'none';
+}
 
 //MOBILE NAV JQUERY END
-
-
+/*
 function mouseOverMoods() {
     moodPicker.classList.add('fade-in');
     moodPicker.style.display = 'flex';
@@ -40,3 +35,4 @@ function mouseOutMoods() {
     moodPicker.classList.remove('fade-in');
     moodPicker.style.display = 'none';
 }
+*/
